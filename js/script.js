d480ee207a106ee4cd22cfc0c148a02ecd91a8ab
Gabriel Salazar <gabriel.salazar777@gmail.com>
2023-04-07 12:16:52 -0400

    faby flies and bounces

--- js/script.js
+++ js/script.js
@@ -11,18 +11,47 @@ let animationId;
 let gameOn = false;
 
 const faby = {
-  x: 600,
-  y: 300,
+  x: 400,
+  y: 200,
   width: 80,
   height: 56,
   speedX: 0,
   speedY: 0,
-  gravity: 0,
+  gravity: 0.2,
   gravitySpeed: 0,
   update() {
+    if (this.y + this.height >= canvas.height) {
+      this.speedY = -5;
+    }
+
+    if (this.y <= 0) {
+      this.y += 10;
+    }
+
+    if (this.speedY < 2) {
+      this.speedY += this.gravity;
+    } else {
+      this.speedY = this.speedY;
+    }
+    this.y += this.speedY;
     ctx.drawImage(fabyImg, this.x, this.y, this.width, this.height);
   },
-  newPosition() {},
+  newPosition(event) {
+    switch (event.code) {
+      case "ArrowLeft":
+        this.x -= 6;
+        break;
+      case "ArrowRight":
+        this.x += 6;
+        break;
+      case "Space":
+        if (this.speedY > -5) {
+          console.log("space");
+          this.speedY -= 1;
+        }
+        break;
+    }
+  },
 };
 
 function animationLoop() {
@@ -55,4 +84,7 @@ window.onload = function () {
       startGame();
     }
   };
+  document.addEventListener("keydown", (e) => {
+    faby.newPosition(e);
+  });
 };
