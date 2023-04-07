471aea19ec0ed442d504c1d860db7c4ffe785794
Gabriel Salazar <gabriel.salazar777@gmail.com>
2023-04-07 11:17:29 -0400

    faby appearing

--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
 const canvas = document.getElementById("my-canvas");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 
 const background = new Image();
-background.src = '/images/bg.png';
+background.src = "/images/bg.png";
+
+const fabyImg = new Image();
+fabyImg.src = "/images/flappy.png";
 
 let animationId;
 let gameOn = false;
 
+const faby = {
+  x: 600,
+  y: 300,
+  width: 80,
+  height: 56,
+  speedX: 0,
+  speedY: 0,
+  gravity: 0,
+  gravitySpeed: 0,
+  update() {
+    ctx.drawImage(fabyImg, this.x, this.y, this.width, this.height);
+  },
+  newPosition() {},
+};
+
 function animationLoop() {
   ctx.clearRect(0, 0, 1200, 600);
   ctx.drawImage(background, 0, 0, 1200, 600);
+  faby.update();
 }
 
 function startGame() {
   console.log("starting");
   animationId = setInterval(animationLoop, 16);
-};
+}
 
 window.onload = function () {
   document.getElementById("start-button").onclick = function () {
